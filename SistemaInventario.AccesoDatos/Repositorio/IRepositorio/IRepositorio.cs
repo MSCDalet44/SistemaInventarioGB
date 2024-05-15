ce6f8dfa91d672cd67a6e7d8ae156952988b5b1b
MSCDalet44 <isc.villanueva@hotmail.com>
2024-05-15 08:37:31 -0600

    Paginacion

--- SistemaInventario.AccesoDatos/Repositorio/IRepositorio/IRepositorio.cs
+++ SistemaInventario.AccesoDatos/Repositorio/IRepositorio/IRepositorio.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SistemaInventario.Modelos.Espesificaciones;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -17,6 +18,13 @@ namespace SistemaInventario.AccesoDatos.Repositorio.IRepositorio
             bool isTracking = true
             );
 
+        PagesList<T> ObtenerTodosPaginado(Parametros parametros,
+            Expression<Func<T, bool>> filtro = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string incluirPropiedades = null,
+            bool isTracking = true
+            );
+
         Task<T> ObtenerPrimero(
             Expression<Func<T, bool>> filtro = null,
             string incluirPropiedades = null,
@@ -26,6 +34,8 @@ namespace SistemaInventario.AccesoDatos.Repositorio.IRepositorio
         Task Agregar(T entidad);
         void Remover(T entidad);
         void RemoverRango(IEnumerable<T> entidad);
+
+        
 
     }
 }
--- SistemaInventario.AccesoDatos/Repositorio/Repositorio.cs
+++ SistemaInventario.AccesoDatos/Repositorio/Repositorio.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SistemaInventario.AccesoDatos.Data;
 using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventario.Modelos.Espesificaciones;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -61,6 +62,37 @@ namespace SistemaInventario.AccesoDatos.Repositorio
         }
 
 
+        public PagesList<T> ObtenerTodosPaginado(Parametros parametros, Expression<Func<T, bool>> filtro = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string incluirPropiedades = null, bool isTracking = true)
+        {
+            IQueryable<T> query = dbSet;
+            if (filtro != null)
+            {
+                query = query.Where(filtro); //Select * from tabla where......
+            }
+
+            if (incluirPropiedades != null)
+            {
+                foreach (var incluirProp in incluirPropiedades.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(incluirProp);
+                }
+            }
+
+            if (orderBy != null)
+            {
+                query = orderBy(query);
+            }
+
+            if (!isTracking)
+            {
+                query = query.AsNoTracking();
+            }
+
+            return PagesList<T>.ToPagesList(query, 
+                                            parametros.PageNumber, 
+                                            parametros.PageSize);
+        }
+
         public async Task<T> ObtenerPrimero(Expression<Func<T, bool>> filtro = null, string incluirPropiedades = null, bool isTracking = true)
         {
             IQueryable<T> query = dbSet;
@@ -96,5 +128,7 @@ namespace SistemaInventario.AccesoDatos.Repositorio
         {
            dbSet.RemoveRange(entidad);
         }
+
+        
     }
 }
